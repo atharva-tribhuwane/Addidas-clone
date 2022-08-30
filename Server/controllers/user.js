c01646dbcedc6e145ed40530fbb43b4500d597e6
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-08-30 09:56:18 +0530

    sai

---
+++ Server/controllers/user.js
@@ -0,0 +1,27 @@
+ const express = require("express");
+
+
+ async function createuser(req,res){
+console.log(req.body)
+
+
+
+ }
+ async function login(req,res){
+    console.log(req.body)
+    
+    
+    
+     }
+
+    
+     
+     async function logged(req,res){
+        console.log(req.body)
+        
+        
+        
+         }
+ module.exports={
+    logged,login,createuser
+ }
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,15 @@
 const express =require("express")
 const bodyparser=require("body-parser")
 const dataconnect= require('./data/index')
+const {
+    logged,login,createuser
+ }= require('./controllers/user')
 const app= express()
+app.use(bodyparser.json())
 
+app.post('/reg',createuser)
+app.post('/log',login)
+app.post('/reg',logged)
 
 
 
