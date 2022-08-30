a4ae3162401a1e219375c5ada69aea5ad8614a06
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-08-30 09:37:52 +0530

    createschema s and routing
    createschema s and routing

---
+++ Server/data/index.js
@@ -0,0 +1,21 @@
+ const mongoose = require('mongoose')
+
+
+ async function condatabase(){
+     return new Promise((resolve, reject)=>{
+        const url='mongodb://localhost:27017/deen'
+        mongoose.connect(url,(err)=>{
+
+            if(err){
+                console.log(err)
+                return reject(err);
+            }
+
+            console.log("database connceted")
+            resolve()
+        })
+
+     })
+ }
+
+ module.exports=condatabase
