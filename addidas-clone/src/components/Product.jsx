786401a7620e028f8e5efecd1a97f7fc473bb4ec
Atharva Tribhuwane <100659665+atharva-tribhuwane@users.noreply.github.com>
2022-09-05 02:45:29 +0530

    adding some changes in product and wishlistprod

--- addidas-clone/src/components/Product.jsx
+++ addidas-clone/src/components/Product.jsx
@@ -8,23 +8,27 @@ import { Link } from 'react-router-dom';
 export const Product = ({prop}) => {
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
   const navigate = useNavigate();
-  console.log("dt:",prop);
+  // console.log("dt:",prop);
   const handleClick = ()=>{
     console.log("clicked")
     let id = prop._id;
     navigate(`/prod/:${id}`);
   }
+
+  const addToWishlist = ()=>{
+    // console.log("")
+  }
   return (
-    <div className='at-producttt' onClick={handleClick}>
+    <div className='at-producttt' >
         <div>
-          <img src={prop.pro_img[0]} alt="" className='at-productimg'/>
-          <span>{`₹ ${prop.price}`}</span>
+          <img src={prop.pro_img[0]} alt="" className='at-productimg' onClick={handleClick}/>
+          <span onClick={handleClick}>{`₹ ${prop.price}`}</span>
           <div className="checkb">
-            <Checkbox {...label} icon={<FavoriteBorder style={{color:"black"}}/>} checkedIcon={<Favorite style={{color:"red"}} />} onClick={()=>alert("Added to wishlist")}/>
+            <Checkbox {...label} icon={<FavoriteBorder style={{color:"black"}}/>} checkedIcon={<Favorite style={{color:"red"}} />} onClick={()=>addToWishlist}/>
           </div>
         </div>
-        <div  className="at-product-names">{prop.pro_name}</div>
-        <div  className="at-product-names">{prop.categeory} • {prop.sub_cat}</div>
+        <div  className="at-product-names" onClick={handleClick}>{prop.pro_name}</div>
+        <div  className="at-product-names" onClick={handleClick}>{prop.categeory} • {prop.sub_cat}</div>
         
     </div>
   )
