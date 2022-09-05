a6d55ad941fcc94bff920b22cd2c470aa3e92f08
lawandepranav <lawandepranav@gmail.com>
2022-09-05 12:35:50 +0530

    Update Whatshot.jsx

--- addidas-clone/src/Adidas/Home/Sliders/Whatshot.jsx
+++ addidas-clone/src/Adidas/Home/Sliders/Whatshot.jsx
@@ -77,8 +77,14 @@ const Whatshot = () => {
           </div>
           <div className="card-main-wrapper-whatshot">
             <div className="card-img-price-div">
-              <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/enIN/Images/1050x1400TC_tcm209-932324.jpg" alt="Forum Low Shoes" />
-              <div className="card-price-div">
+            <ReactPlayer
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+            url="https://brand.assets.adidas.com/video/upload/q_auto,vc_auto,c_scale,w_0.5/video/upload/global%20brand%20publishing/Running/fw22-tinman-elite/running-fw22-tinman-elite-launch-com-hp-teaser-carousel-d.mp4"
+          /> <div className="card-price-div">
               </div>
             </div>
             <div className="card-other-details"></div>
